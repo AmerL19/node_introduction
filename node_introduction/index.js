1e419050b92752b5f28014f65c2f576c1956db9b
Amer Loubani <loubania@aston.ac.uk>
2019-02-01 16:46:57 +0000

    second ting

--- node_introduction/index.js
+++ node_introduction/index.js
@@ -1,1 +1,12 @@
 console.log('index.js executing');
+
+var express = require('express');
+var app = express();
+
+app.get('/',function(req,res){
+res.send('hello there!');
+});
+var port = 3000;
+app.listen(port,function(){
+console.log('listening on port ' + port);
+});
